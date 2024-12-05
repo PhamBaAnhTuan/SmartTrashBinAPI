fc5a3167af8edbc6c70cfa5c391495bc3b60a9ce
Phạm Bá Anh Tuấn <96640490+PhamBaAnhTuan@users.noreply.github.com>
2024-12-05 22:48:05 +0700

    Delete SmartTrashBinAPI/trash directory

--- SmartTrashBinAPI/trash/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TrashConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'trash'
--- SmartTrashBinAPI/trash/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Trash(models.Model):
-   organic = models.DecimalField(max_digits=5, decimal_places=1, null=True, blank=True)
-   inOrganic = models.DecimalField(max_digits=5, decimal_places=1, null=True, blank=True)
-   
-class TrashType(models.Model):
-   name = models.CharField(max_length=50)
